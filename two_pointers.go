099da2894f31af247aaa06b0f55b445f0439da53
harsha <harshav17@gmail.com>
2020-04-09 22:32:51 -0700

    squares and zeros

--- two_pointers.go
+++ two_pointers.go
@@ -1,5 +1,9 @@
 package educativeingo
 
+import (
+	"sort"
+)
+
 //[1, 2, 3, 4, 6], target=6
 //there is also another with hashtable approach for this
 func pairWithTargerSum(arr []int, targetSum int) []int {
@@ -30,4 +34,53 @@ func removeDuplicates(arr []int) int {
 		j++
 	}
 	return len(arr[:i+1])
+}
+
+func sorterdArraySquares(arr []int) []int {
+	result := make([]int, len(arr))
+	resultIdx := len(arr) - 1
+	i := 0
+	j := len(arr) - 1
+	for i < j {
+		i2 := arr[i] * arr[i]
+		j2 := arr[j] * arr[j]
+		if i2 < j2 {
+			result[resultIdx] = j2
+			j--
+		} else {
+			result[resultIdx] = i2
+			i++
+		}
+		resultIdx--
+	}
+	return result
+}
+
+func tripletSumToZero(arr []int) [][3]int {
+	var result [][3]int
+	sort.Ints(arr)
+	for i, targetSum := range arr {
+		j := i + 1
+		k := len(arr) - 1
+		for j < k {
+			if arr[j]+arr[k] == -targetSum {
+				result = append(result, [3]int{targetSum, arr[j], arr[k]})
+				j++
+				k--
+				for j < k && arr[j] == arr[j-1] {
+					j++
+				}
+				for j < k && arr[k] == arr[k+1] {
+					k--
+				}
+			}
+			if arr[j]+arr[k] > -targetSum {
+				k--
+			}
+			if arr[j]+arr[k] < -targetSum {
+				j++
+			}
+		}
+	}
+	return result
 }
--- two_pointers_test.go
+++ two_pointers_test.go
@@ -39,7 +39,6 @@ func Test_removeDuplicates(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.2, 2, 2, 11
 		{
 			name: "sanity2",
 			args: args{[]int{2, 3, 3, 3, 6, 9, 9}},
@@ -55,6 +54,54 @@ func Test_removeDuplicates(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := removeDuplicates(tt.args.arr); got != tt.want {
 				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sorterdArraySquares(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "sanity2",
+			args: args{[]int{-2, -1, 0, 2, 3}},
+			want: []int{0, 1, 4, 4, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sorterdArraySquares(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sorterdArraySquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_tripletSumToZero(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][3]int
+	}{
+		{
+			name: "sanity2",
+			args: args{[]int{-3, 0, 1, 2, -1, 1, -2}},
+			want: [][3]int{{-3, 1, 2}, {-2, 0, 2}, {-2, 1, 1}, {-1, 0, 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tripletSumToZero(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("tripletSumToZero() = %v, want %v", got, tt.want)
 			}
 		})
 	}
