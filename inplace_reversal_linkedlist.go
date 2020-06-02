f79f9ba38d79b654617eae6d88a9531ef1588deb
harsha <harshav17@gmail.com>
2020-06-01 19:41:09 -0700

    final linked lists

--- inplace_reversal_linkedlist.go
+++ inplace_reversal_linkedlist.go
@@ -29,3 +29,80 @@ func reverseASubList(root *node, p int, q int) *node {
 
 	return root
 }
+
+func reverseEveryKSublist(root *node, k int) *node {
+	var prev *node
+	current := root
+
+	for current != nil {
+		lastNodeOfFirst := prev
+		lastNodeOfSubList := current
+
+		for i := 0; current != nil && i < k; i++ {
+			next := current.next
+			current.next = prev
+			prev = current
+			current = next
+		}
+
+		if lastNodeOfFirst != nil {
+			lastNodeOfFirst.next = prev
+		} else {
+			root = prev
+		}
+		lastNodeOfSubList.next = current
+		prev = lastNodeOfSubList
+	}
+
+	return root
+}
+
+func reverseAlternatingKSublist(root *node, k int) *node {
+	var prev *node
+	current := root
+
+	for current != nil {
+		lastNodeOfFirst := prev
+		lastNodeOfSubList := current
+
+		for i := 0; current != nil && i < k; i++ {
+			next := current.next
+			current.next = prev
+			prev = current
+			current = next
+		}
+
+		if lastNodeOfFirst != nil {
+			lastNodeOfFirst.next = prev
+		} else {
+			root = prev
+		}
+
+		lastNodeOfSubList.next = current
+		for j := 0; current != nil && j < k; j++ {
+			prev = current
+			current = current.next
+		}
+	}
+	return root
+}
+
+func rotateALinkedList(root *node, rotations int) *node {
+	lastNode := root
+	listLen := 1
+	for lastNode.next != nil {
+		lastNode = lastNode.next
+		listLen++
+	}
+	lastNode.next = root
+	rotations %= listLen
+	skipLen := listLen - rotations
+	rotNode := root
+	for i := 0; i < skipLen-1; i++ {
+		rotNode = rotNode.next
+	}
+
+	root = rotNode.next
+	rotNode.next = nil
+	return root
+}
--- inplace_reversal_linkedlist_test.go
+++ inplace_reversal_linkedlist_test.go
@@ -34,3 +34,90 @@ func Test_reverseASubList(t *testing.T) {
 		})
 	}
 }
+
+func Test_reverseEveryKSublist(t *testing.T) {
+	type args struct {
+		root *node
+		k    int
+	}
+
+	input := node{7, &node{6, &node{5, &node{4, &node{3, &node{2, &node{1, nil}}}}}}}
+	output := node{5, &node{6, &node{7, &node{2, &node{3, &node{4, &node{1, nil}}}}}}}
+
+	tests := []struct {
+		name string
+		args args
+		want *node
+	}{
+		{
+			name: "sanity",
+			args: args{root: &input, k: 3},
+			want: &output,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseEveryKSublist(tt.args.root, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseEveryKSublist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseAlternatingKSublist(t *testing.T) {
+	type args struct {
+		root *node
+		k    int
+	}
+
+	input := node{8, &node{7, &node{6, &node{5, &node{4, &node{3, &node{2, &node{1, nil}}}}}}}}
+	output := node{7, &node{8, &node{6, &node{5, &node{3, &node{4, &node{2, &node{1, nil}}}}}}}}
+
+	tests := []struct {
+		name string
+		args args
+		want *node
+	}{
+		{
+			name: "sanity",
+			args: args{root: &input, k: 2},
+			want: &output,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseAlternatingKSublist(tt.args.root, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseAlternatingKSublist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_rotateALinkedList(t *testing.T) {
+	type args struct {
+		root      *node
+		rotations int
+	}
+
+	input := node{6, &node{5, &node{4, &node{3, &node{2, &node{1, nil}}}}}}
+	output := node{3, &node{2, &node{1, &node{6, &node{5, &node{4, nil}}}}}}
+
+	tests := []struct {
+		name string
+		args args
+		want *node
+	}{
+		{
+			name: "sanity",
+			args: args{root: &input, rotations: 3},
+			want: &output,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rotateALinkedList(tt.args.root, tt.args.rotations); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rotateALinkedList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
